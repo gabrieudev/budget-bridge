3a78ebef663d58bf828a4854a2cec7d22b4d8da3
João Gabriel da Conceição Santos <gabrieudev@gmail.com>
2024-10-16 23:12:58 -0300

    feat: create configuration files

---
+++ backend/budget-bridge/src/main/java/com/api/budget_bridge/config/swagger/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.api.budget_bridge.config.swagger;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI budgetBridgeOpenAPI(){
+        return new OpenAPI()
+                .info(new Info().title("Budget Bridge")
+                        .description("Budget Bridge REST API")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("GitHub Repository")
+                        .url("https://github.com/gabrieudev/budget-bridge/"));
+    }
+}
