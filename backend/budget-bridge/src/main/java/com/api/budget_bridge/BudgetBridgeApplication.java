fd34aed85428b0c730012e5d25d08a8b0151fb7d
João Gabriel da Conceição Santos <gabrieudev@gmail.com>
2024-10-16 23:12:02 -0300

    first commit (backend)

---
+++ backend/budget-bridge/src/main/java/com/api/budget_bridge/BudgetBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.api.budget_bridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetBridgeApplication.class, args);
+	}
+
+}
---
+++ backend/budget-bridge/src/test/java/com/api/budget_bridge/BudgetBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.budget_bridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetBridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
