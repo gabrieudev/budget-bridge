958de631013c8867ab09465aecf9e10dd470276d
João Gabriel <gabrieudev@gmail.com>
2025-03-20 12:31:04 -0300

    feat: configure next auth

---
+++ budget-bridge-frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,81 @@
+import { AuthOptions, TokenSet } from "next-auth";
+import KeycloakProvider from "next-auth/providers/keycloak";
+import NextAuth from "next-auth/next";
+import { JWT } from "next-auth/jwt";
+
+function requestRefreshOfAccessToken(token: JWT) {
+  return fetch(`${process.env.KEYCLOAK_ISSUER}/protocol/openid-connect/token`, {
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams({
+      client_id: process.env.KEYCLOAK_CLIENT_ID,
+      client_secret: process.env.KEYCLOAK_CLIENT_SECRET,
+      grant_type: "refresh_token",
+      refresh_token: token.refreshToken! as string,
+    }),
+    method: "POST",
+    cache: "no-store",
+  });
+}
+
+export const authOptions: AuthOptions = {
+  providers: [
+    KeycloakProvider({
+      clientId: process.env.KEYCLOAK_CLIENT_ID,
+      clientSecret: process.env.KEYCLOAK_CLIENT_SECRET,
+      issuer: process.env.KEYCLOAK_ISSUER,
+    }),
+  ],
+  pages: {
+    signIn: "/auth/signin",
+    signOut: "/auth/signout",
+  },
+  session: {
+    maxAge: 60 * 30,
+  },
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.idToken = account.id_token;
+        token.accessToken = account.access_token;
+        token.refreshToken = account.refresh_token;
+        token.expiresAt = account.expires_at;
+      }
+
+      if (typeof token.expiresAt === "number") {
+        if (Date.now() < token.expiresAt * 1000 - 60 * 1000) {
+          return token;
+        }
+      }
+
+      try {
+        const response = await requestRefreshOfAccessToken(token);
+
+        const tokens: TokenSet = await response.json();
+
+        if (!response.ok) throw tokens;
+
+        const updatedToken: JWT = {
+          ...token,
+          idToken: tokens.id_token,
+          accessToken: tokens.access_token,
+          expiresAt: Math.floor(
+            Date.now() / 1000 + (tokens.expires_in as number)
+          ),
+          refreshToken: tokens.refresh_token ?? token.refreshToken,
+        };
+        return updatedToken;
+      } catch (error) {
+        console.error("Error refreshing access token", error);
+        return { ...token, error: "RefreshAccessTokenError" };
+      }
+    },
+    async session({ session, token }) {
+      (session as any).accessToken = token.accessToken;
+      return session;
+    },
+  },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
